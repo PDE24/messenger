efa20f12661a8ebef78152c15332574be0fe1c96
Euan Macfarlane <euanmacfarlane@macbook-pro.lan>
2022-08-02 15:10:06 +0100

    sets up basic react app

---
+++ messenger/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">MESSENGER APP</header>
+    </div>
+  );
+}
+
+export default App;
